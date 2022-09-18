7afd8bc3760479f3e92e18204880278ca1d75a11
Alejo Verhagen <verhagenalejo@gmail.com>
2022-09-18 18:56:03 -0300

    titulo de novedades y correccion de cajas

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,12 +4,15 @@ import '../styles/components/pages/HomePage.css'
 const HomePage = (porps) => {
     return(
         <main className="holder">
-            <div className="carta">
-                <img className="portada" src="img/Comics/Venom-01.jpg" alt="Venom 01" />
-                <h3>Venom 01</h3>
-                <h3>Rex</h3>
-                <h4>$1100</h4>
-            </div>
+            <section>
+                <h2>Novedades</h2>
+                <div className="carta">
+                    <img className="portada" src="img/Comics/Venom-01.jpg" alt="Venom 01" />
+                    <h3>Venom 01</h3>
+                    <h3>Rex</h3>
+                    <h4>$1100</h4>
+                </div>
+            </section>
         </main>
     );
 }
