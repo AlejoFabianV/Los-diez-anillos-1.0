5b5aaa77cfc86026e0ee9a7c6d3b81951c1e5a59
Alejo Verhagen <verhagenalejo@gmail.com>
2022-09-20 20:37:34 -0300

    agregue "end" para desactivar el nav home

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -6,7 +6,7 @@ const Nav = (props) => {
     return(
         <nav>
             <ul className="holder">
-                <li><NavLink className={({ isActive }) => isActive ? "active" : undefined} to="/">Home</NavLink></li>
+                <li><NavLink className={({ isActive }) => isActive ? "active" : undefined} to="/" end>Home</NavLink></li>
                 <li><NavLink className={({ isActive }) => isActive ? "active" : undefined} to="/tienda">Tienda</NavLink></li>
                 <li><NavLink className={({ isActive }) => isActive ? "active" : undefined} to="/contacto">Contacto</NavLink></li>
                 <li><NavLink className={({ isActive }) => isActive ? "active" : undefined} to="/cuenta">Cuenta</NavLink></li>
