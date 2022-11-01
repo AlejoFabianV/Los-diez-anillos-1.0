db44a6f10d29f5d6480e5bcc8919ee9605b2a170
Alejo Verhagen <verhagenalejo@gmail.com>
2022-11-01 14:33:14 -0300

    identacion del codigo

--- src/pages/ContactoPage.js
+++ src/pages/ContactoPage.js
@@ -4,27 +4,27 @@ import '../styles/components/pages/ContactoPage.css'
 const ContactoPage = (props) => {
     return(
         <main className="holder">
-        <div className="formulario-contacto">
-            <h2 className="texto-contacto">Contactanos y pregunta por tus comics/mangas favoritos</h2>
-            <form action="">
-                <p>
-                    <label>Nombre</label>
-                    <input type="text" />
-                </p>
-                <p>
-                    <label>Email</label>
-                    <input type="email" />
-                </p>
-                <p>
-                    <label>Mensaje</label>
-                    <textarea name="" id="" cols="30" rows="10" />
-                </p>
-                <p>
-                    <input type="submit" />
-                </p>
-            </form>
-        </div>
-    </main>
+            <div className="formulario-contacto">
+                <h2 className="texto-contacto">Contactanos y pregunta por tus comics/mangas favoritos</h2>
+                <form action="">
+                    <p>
+                        <label>Nombre</label>
+                        <input type="text" />
+                    </p>
+                    <p>
+                        <label>Email</label>
+                        <input type="email" />
+                    </p>
+                    <p>
+                        <label>Mensaje</label>
+                        <textarea name="" id="" cols="30" rows="10" />
+                    </p>
+                    <p>
+                        <input type="submit" />
+                    </p>
+                </form>
+            </div>
+        </main>
     );
 }
 export default ContactoPage;
--- src/pages/CuentaPage.js
+++ src/pages/CuentaPage.js
@@ -4,26 +4,26 @@ import '../styles/components/pages/CuentaPage.css'
 const CuentaPage = (props) => {
     return(
         <main className="holder">
-        <div className="formulario-cuenta">
-            <h2>Cuenta</h2>
-            <form action="">
-                <p>
-                    <label>Nombre de cuenta o Email</label>
-                    <input type="text , email" />
-                </p>
-                <p>
-                    <label>Contaseña</label>
-                    <input type="password" />
-                </p>
-                <p>
-                    <button className="boton-login">Login</button>
-                    <br />
-                    <br />
-                    <button className="boton-register">Register</button>
-                </p>
-            </form>
-        </div>
-    </main>
+            <div className="formulario-cuenta">
+                <h2>Cuenta</h2>
+                <form action="">
+                    <p>
+                        <label>Nombre de cuenta o Email</label>
+                        <input type="text , email" />
+                    </p>
+                    <p>
+                        <label>Contaseña</label>
+                        <input type="password" />
+                    </p>
+                    <p>
+                        <button className="boton-login">Login</button>
+                        <br />
+                        <br />
+                        <button className="boton-register">Register</button>
+                    </p>
+                </form>
+            </div>
+        </main>
     );
 }
 export default CuentaPage;
