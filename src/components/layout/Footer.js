f1b11d3a0fcd53e64d20c00a0b1ce2ce7f10b42e
Alejo Verhagen <verhagenalejo@gmail.com>
2022-11-29 23:08:30 -0300

    coreccion en errores de tipeo

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import '../../styles/components/layout/Footer.css'
 
-const Footer = (porps) => {
+const Footer = (props) => {
     return(
         <footer>
             <div className="holder">
