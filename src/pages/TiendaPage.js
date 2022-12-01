99ef7103c5c30d76e08886659299f441d7bf8479
Alejo Verhagen <verhagenalejo@gmail.com>
2022-12-01 20:05:46 -0300

    consumiendo api novedades

--- src/pages/TiendaPage.js
+++ src/pages/TiendaPage.js
@@ -1,134 +1,44 @@
 import React from "react";
 import '../styles/components/pages/TiendaPage.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import NovedadesItem from "../components/novedades/NovedadItem";
 
 const TiendaPage = (props) => {
-    return(
+
+    const [loading, setLoading] = useState(false);
+    const [novedades, setNovedades] = useState([]);
+
+    useEffect(() => {
+        const cargarNovedades = async () => {
+            setLoading(true);
+            const response = await axios.get('http://localhost:3000/api/novedades');
+            setNovedades(response.data);
+            setLoading(false);
+        };
+
+        cargarNovedades();
+    }, []);
+
+    return (
         <main className="holder-tienda">
             <section>
-                <h2>Comics</h2>
-                <div className="carta-tienda">
-                    <img className="portada-tienda" src="img/Comics/Avengers-llegada-del-fenix.jpg" alt="Avengers 06-llegada-del-fenix" />
-                    <h3>Avengers 06</h3>
-                    <h3>La llegada del fenix</h3>
-                    <h4>$1400</h4>
-                </div>
-                <div className="carta">
-                    <img className="portada" src="img/Comics/Venom-01.jpg" alt="Venom 01" />
-                    <h3>Venom 01</h3>
-                    <h3>Rex</h3>
-                    <h4>$1100</h4>
-                </div>
-                <div className="carta">
-                    <img className="portada" src="img/Comics/cap-america-todos-mueren-jovenes.jpg" alt="capitan america 04" />
-                    <h3>Capitan America 04</h3>
-                    <h3>Todos mueren jovenes</h3>
-                    <h4>$1100</h4>
-                </div>
-                <div className="carta">
-                    <img className="portada" src="img/Comics/miles-morales01.jpg" alt="miles morales 01" />
-                    <h3>Miles Morales 01</h3>
-                    <h3>Spider-Man</h3>
-                    <h4>$2000</h4>
-                </div>
-                <div className="carta">
-                    <img className="portada" src="img/Comics/thor-05.jpg" alt="Thor 05 el rey devorador" />
-                    <h3>Thor 05</h3>
-                    <h3>El rey devorador</h3>
-                    <h4>$1300</h4>
-                </div>
-                <div className="carta">
-                    <img className="portada" src="img/Comics/cap-america03.jpg" alt="cap ameica 03 La leyenda de steve" />
-                    <h3>Capitan Ameica 03</h3>
-                    <h3>La leyenda de Steve</h3>
-                    <h4>$1200</h4>
-                </div>
-                <div className="carta">
-                    <img className="portada" src="img/Comics/xmen-15-amenecer-p11.jpg" alt="x-men amanecer x parte 11" />
-                    <h3>X-men 15</h3>
-                    <h3>Amanecer x parte 11</h3>
-                    <h4>$1300</h4>
-                </div>
-                <div className="carta">
-                    <img className="portada" src="img/Comics/venom-06.jpg" alt="venom-06 isla Venom" />
-                    <h3>Venom 06</h3>
-                    <h3>Isla Venom</h3>
-                    <h4>$1100</h4>
-                </div>
-                <div className="carta">
-                    <img className="portada" src="img/Comics/valkyria-janeFoster-01.jpg" alt="valkyria janeFoster-01 lo sagrado y lo profano" />
-                    <h3>Valkyria Jane Foster 01</h3>
-                    <h3>Lo sagrado y lo profano</h3>
-                    <h4>$1400</h4>
-                </div>
-                <div className="carta">
-                    <img className="portada" src="img/Comics/xmen-11-amanecer-07.jpg" alt="xmen 11 amanecer x parte 07" />
-                    <h3>X-men 11</h3>
-                    <h3>Amanecer x parte 07</h3>
-                    <h4>$1200</h4>
-                </div>
-            </section>
-            <section>
-                <h2>Manga</h2>
-                <div className="carta">
-                    <img className="portada" src="img/Manga/jujutsu-kaisen-11.jpg" alt="jujutsu-kaisen-11" />
-                    <h3>Jujutsu Kaisen 11</h3>
-                    <h3>Gege Akutami</h3>
-                    <h4>$1100</h4>
-                </div>
-                <div className="carta">
-                    <img className="portada" src="img/Manga/dr.stone-17.jpg" alt="dr.stone-17" />
-                    <h3>Dr.Stone 17</h3>
-                    <h3>Riichiro Inagaki</h3> 
-                    <h4>$1100</h4>
-                </div>
-                <div className="carta">
-                    <img className="portada" src="img/Manga/naruto-06.jpg" alt="naruto-06" />
-                    <h3>Naruto 06</h3>
-                    <h3>Masashi Kishimoto</h3>
-                    <h4>$1100</h4>
-                </div>
-                <div className="carta">
-                    <img className="portada" src="img/Manga/fire-force-15.jpg" alt="fire-force-15" />
-                    <h3>Fire Force 15</h3>
-                    <h3>Atsushi Ohkubo</h3> 
-                    <h4>$1100</h4>
-                </div>
-                <div className="carta">
-                    <img className="portada" src="img/Manga/naruto-10.jpg" alt="naruto-10" />
-                    <h3>Naruto 10</h3>
-                    <h3>Masashi Kishimoto</h3>
-                    <h4>$1100</h4>
-                </div>
-                <div className="carta">
-                    <img className="portada" src="img/Manga/jujutsu-kaisen-13.jpg" alt="jujut-kaisen-13" />
-                    <h3>Jujutsu Kaisen 13</h3>
-                    <h3>Gege Akutami</h3>
-                    <h4>$1100</h4>
-                </div>
-                <div className="carta">
-                    <img className="portada" src="img/Manga/berserk-30.jpg" alt="berserk-30" />
-                    <h3>Berserk 30</h3>
-                    <h3>Kentaro Kiura</h3>
-                    <h4>$1100</h4>
-                </div>
-                <div className="carta">
-                    <img className="portada" src="img/Manga/jujutsu-kaisen-10.jpg" alt="jujutsu-kaisen-10" />
-                    <h3>Jujutsu Kaisen 10</h3>
-                    <h3>Gege Akutami</h3>
-                    <h4>$1100</h4>
-                </div>
-                <div className="carta">
-                    <img className="portada" src="img/Manga/fire-force-13.jpg" alt="fire-force-13" />
-                    <h3>Fire Force 13</h3>
-                    <h3>Atsushi Ohkubo</h3>
-                    <h4>$1100</h4>
-                </div>
-                <div className="carta">
-                    <img className="portada" src="img/Manga/one-piece-42.jpg" alt="one-piece-42" />
-                    <h3>One Piece 42</h3>
-                    <h3>Eiichiro Oda</h3> 
-                    <h4>$750</h4>
-                </div>
+                <h2>Comics y Mangas</h2>
+                {   
+                    loading ? (
+                        <p>Cargando...</p>
+                    ) : (
+                        novedades.map(item => 
+                        <NovedadesItem 
+                            id={item.id} 
+                            title={item.titulo} 
+                            subtitle={item.subtitulo} 
+                            portada={item.portada} 
+                            precio={item.precio} 
+                            key={item.descripcion}
+                        />)
+                    )
+                }
             </section>
         </main>
     );
