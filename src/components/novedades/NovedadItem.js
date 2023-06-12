879cc7a72ba15d9f93c3cb92c9b1962ba532b68c
Alejo Verhagen <verhagenalejo@gmail.com>
2023-06-12 14:52:04 -0300

    alt al img

--- src/components/novedades/NovedadItem.js
+++ src/components/novedades/NovedadItem.js
@@ -6,7 +6,7 @@ function NovedadesItem (props) {
     return (
             <div className='carta'>
                 <Link to={`/producto/${props.title}/${props.subtitle}/${props.id}`}>
-                  <img className='portada' src={props.portada} />
+                  <img className='portada' src={props.portada} alt='Portada' />
                 </Link>
                 <h3>{props.title}</h3>
                 <h3>{props.subtitle}</h3>
